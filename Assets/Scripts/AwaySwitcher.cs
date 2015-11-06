5fe937280f048b6bce97366834281a1a48d5629b
John Lau <john.matthew.lau@gmail.com>
2015-11-06 11:05:25 +0000

    Initial upload
    First upload to repository

---
+++ Assets/Scripts/AwaySwitcher.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AwaySwitcher : MonoBehaviour 
+	{
+	public GameObject[] Cameras;
+
+
+	// Use this for initialization
+	void Start () 
+	
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+				SwitchDialogue (0);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+			SwitchDialogue (1);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+			SwitchDialogue (2);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha4))
+			SwitchDialogue (3);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha5))
+			SwitchDialogue (4);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha6))
+			SwitchDialogue (5);
+			
+		if (Input.GetKeyDown (KeyCode.Alpha7))
+			SwitchDialogue (6);
+			
+	
+	}
+
+	public void SwitchDialogue (int LineNum)
+	{
+		foreach (GameObject GO in Cameras) {
+			GO.SetActive (false);
+		}
+		Cameras[LineNum].SetActive(true);
+	}
+}
---
+++ Assets/Scripts/HomeSwitcher.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeSwitcher : MonoBehaviour 
+{
+
+	public GameObject[] Cameras;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+	{
+	if (Input.GetKeyDown (KeyCode.Keypad1)) 
+		{
+			SwitchCam (0);
+		}
+
+	if (Input.GetKeyDown (KeyCode.Keypad2))
+		{
+			SwitchCam (1);
+		}
+
+	if (Input.GetKeyDown (KeyCode.Keypad3))
+		{
+			SwitchCam (2);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Keypad4))
+		{
+			SwitchCam (3);
+		}
+
+	if (Input.GetKeyDown (KeyCode.Keypad5))
+		{
+			SwitchCam (4);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Keypad6))
+		{
+			SwitchCam (5);
+		}
+
+	if (Input.GetKeyDown (KeyCode.Keypad7)) 
+		{
+			SwitchCam (6);
+		}
+	}
+
+	public void SwitchCam (int CamNum)
+	{
+		foreach (GameObject GO in Cameras) {
+			GO.SetActive (false);
+		}
+		Cameras[CamNum].SetActive(true);
+	}
+}
